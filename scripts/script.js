901e103fe40f82993a76097217a99c095fe3453e
yassir bghiel <yassir.bghiel@hva.nl>
2023-09-08 13:03:03 +0200

    javascript, css en html toegevoegd en veranderd

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,11 @@ expandButton.addEventListener('click', expand)
 function expand () {
   document.body.classList.toggle('expand')
 }
+
+function changeImage() {
+  if (document.getElementById("imgClickAndChange").src == "https://media.discordapp.net/attachments/620743305713680385/1149644670532591687/vacation_3.png?width=1034&height=1034"){
+      document.getElementById("imgClickAndChange").src = "https://media.discordapp.net/attachments/620743305713680385/1149641421188497439/outfitalleen.png?width=1034&height=1034";
+  } else {
+      document.getElementById("imgClickAndChange").src = "https://media.discordapp.net/attachments/620743305713680385/1149644670532591687/vacation_3.png?width=1034&height=1034";
+  }
+}
